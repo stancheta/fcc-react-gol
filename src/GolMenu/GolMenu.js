a73434efc11debe597988e1ebbb2a51e16c5bec5
Steven <steventancheta@gmail.com>
2016-10-03 20:42:34 -0700

    Created Mockup of basic app shape and headers

--- src/GolMenu/GolMenu.js
+++ src/GolMenu/GolMenu.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './GolMenu.sass';
 
+const GolMenuHeader = function(props) {
+  return (
+    <div  className="header">
+      <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">
+        <h1>Game of Life</h1>
+      </a>
+      <hr />
+    </div>
+  );
+};
+
 const GolMenu = React.createClass({
   render: function() {
     return (
-      <div className="GolMenu">
-
+      <div className="GolMenu section">
+        <GolMenuHeader />
       </div>
     );
   }
--- src/GolScreen/GolScreen.js
+++ src/GolScreen/GolScreen.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './GolScreen.sass';
 
+const GolScreenHeader = function(props) {
+  return (
+    <div  className="header">
+      <h2>Generation: {props.generation}</h2>
+    </div>
+  );
+};
+
 const GolScreen = React.createClass({
   render: function() {
     return (
-      <div className="GolScreen">
-
+      <div className="GolScreen section">
+        <GolScreenHeader
+          generation={0}
+        />
+        <hr />
       </div>
     );
   }
